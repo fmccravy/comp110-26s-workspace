31584bff89d54b289257a62e4438f4aec00de784
Faith Mccravy <fmccravy@unc.edu>
2026-04-28 00:52:15 +0000

    initial site setup

--- exercises/ex09/data_utils.py
+++ exercises/ex09/data_utils.py
@@ -1,94 +1,104 @@
 """Data related utility functions."""
 
-__author__ = ["", ""]
+__author__ = "730941956"
 
 from csv import DictReader
 
 
-def get_keys(
-    input_dict: (
-        dict[str, list[str]]
-        | dict[str, list[int]]
-        | dict[str, list[str | int]]
-        | dict[str, int]
-        | dict[str, str]
-    ),
-) -> list[str]:
+def get_keys(input_dict: dict) -> list[str]:
+    """Returns a list of all keys in a dictionary."""
     result: list[str] = []
     for key in input_dict:
         result.append(key)
-
     return result
 
 
-def convert_columns_to_int(
-    data: dict[str, list[str]], columns_conv: list[str]
-) -> dict[str, list[str | int]]:
-    """Convert the data in the selected columns to be of type int."""
-    # Create new table to store converted data
-    data_converted: dict[str, list[int | str]] = {}
-
-    # Iterate through column names (keys of the dictionary)
-    for col_name in data:
-        # Create new list to append converted values to
-        data_converted[col_name] = []
-
-        # Declare the converted value with a type of either int or str
-        converted_value: int | str
-
-        # Iterate through data values in the column
-        for value in data[col_name]:
-            # If this column is in the list of columns to be converted,
-            # cast it to an int
-            if col_name in columns_conv:
-                converted_value = int(value)
-            else:
-                converted_value = value
-
-            # Add it to the new column of values, the list we created
-            # that we have a reference to at data_converted[col_name]
-            data_converted[col_name].append(converted_value)
-
-    return data_converted
-
-"""These are the functions we wrote/will write in class!"""
 def read_csv_rows(filename: str) -> list[dict[str, str]]:
-    """Read the rows of a CSV into a 'table'."""
+    """Read an entire CSV file of data into a list of rows."""
     result: list[dict[str, str]] = []
-
-    # Open a handle to the data file
     file_handle = open(filename, "r", encoding="utf8")
-
-    # Prepare to read the data file as a CSV rather than just strings.
     csv_reader = DictReader(file_handle)
-
-    # Read each row of the CSV line-by-line
     for row in csv_reader:
         result.append(row)
-
-    # Close the file when done, to free its resources.
     file_handle.close()
-
     return result
 
 
 def column_values(table: list[dict[str, str]], column: str) -> list[str]:
     """Produce a list[str] of all values in a single column."""
     result: list[str] = []
-
     for row in table:
         item: str = row[column]
         result.append(item)
-
     return result
 
 
 def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
     """Transform a row-oriented table to a column-oriented table."""
     result: dict[str, list[str]] = {}
-
+    if len(row_table) == 0:
+        return result
     first_row: dict[str, str] = row_table[0]
     for column in first_row:
         result[column] = column_values(row_table, column)
-
     return result
+
+
+def head(table: dict[str, list[str]], n: int) -> dict[str, list[str]]:
+    """Produce a new table with only the first n rows of data."""
+    result: dict[str, list[str]] = {}
+    for column in table:
+        first_n_items: list[str] = []
+        for i in range(min(n, len(table[column]))):
+            first_n_items.append(table[column][i])
+        result[column] = first_n_items
+    return result
+
+
+def select(table: dict[str, list[str]], names: list[str]) -> dict[str, list[str]]:
+    """Produce a new table with only a specific subset of columns."""
+    result: dict[str, list[str]] = {}
+    for name in names:
+        result[name] = table[name]
+    return result
+
+
+def concat(
+    table1: dict[str, list[str]], table2: dict[str, list[str]]
+) -> dict[str, list[str]]:
+    """Combine two column-based tables into one."""
+    result: dict[str, list[str]] = {}
+    for column in table1:
+        result[column] = table1[column]
+    for column in table2:
+        if column in result:
+            result[column] += table2[column]
+        else:
+            result[column] = table2[column]
+    return result
+
+
+def count(values: list[str]) -> dict[str, int]:
+    """Count the frequency of each unique value in a list."""
+    result: dict[str, int] = {}
+    for item in values:
+        if item in result:
+            result[item] += 1
+        else:
+            result[item] = 1
+    return result
+
+
+def convert_columns_to_int(
+    data: dict[str, list[str]], columns_conv: list[str]
+) -> dict[str, list[str | int]]:
+    """Convert strings in selected columns to integers."""
+    data_converted: dict[str, list[int | str]] = {}
+    for col_name in data:
+        data_converted[col_name] = []
+        for value in data[col_name]:
+            if col_name in columns_conv:
+                data_converted[col_name].append(int(value))
+            else:
+                data_converted[col_name].append(value)
+    return data_converted
